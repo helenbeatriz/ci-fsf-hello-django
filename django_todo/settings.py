a57dfcdddbef0b43fd1ff0667e162731a1d130f4
Helen Cantu <helen_beatriz02@outlook.com>
2023-03-02 13:15:47 +0000

    Adding ElephantData database

--- django_todo/settings.py
+++ django_todo/settings.py
@@ -9,8 +9,9 @@ https://docs.djangoproject.com/en/3.2/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.2/ref/settings/
 """
-
 from pathlib import Path
+import dj_database_url
+
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -74,12 +75,24 @@ WSGI_APPLICATION = 'django_todo.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'bimxbcsm',
+        'USER': 'bimxbcsm',
+        'PASSWORD': 'yIfxh8rxM78PvHtTr4vfnpz-Fj835fUb',
+        'HOST': 'trumpet.db.elephantsql.com',
+        'PORT': '5432',
     }
 }
+
 
 
 # Password validation
